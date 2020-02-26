9689672c582c075f6d01cd7e0eec8d7e15c14062
Coldairarrow <862520575@qq.com>
2020-02-26 15:59:02 +0800

    单元测试

--- src/EFCore.Sharding/Sharding/Model/ExpandByDateMode.cs
+++ src/EFCore.Sharding/Sharding/Model/ExpandByDateMode.cs
@@ -1,11 +1,33 @@
 ﻿namespace EFCore.Sharding
 {
+    /// <summary>
+    /// 按时间扩容模式
+    /// </summary>
     public enum ExpandByDateMode
     {
+        /// <summary>
+        /// 每分钟
+        /// </summary>
         PerMinute,
+
+        /// <summary>
+        /// 每小时
+        /// </summary>
         PerHour,
+
+        /// <summary>
+        /// 每天
+        /// </summary>
         PerDay,
+
+        /// <summary>
+        /// 每月
+        /// </summary>
         PerMonth,
+
+        /// <summary>
+        /// 每年
+        /// </summary>
         PerYear
     }
 }
