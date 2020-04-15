7e17d3ae0aed6193f290622fd788f2eef25491b8
光灿 许 <xgc15657519397@outlook.com>
2020-04-15 16:04:34 +0800

    添加注入使用Demo

---
+++ examples/Demo.DI/Program.cs
@@ -0,0 +1,28 @@
+﻿using EFCore.Sharding;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Demo.DI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string conString = "DataSource=db.db";
+
+            Host.CreateDefaultBuilder(args)
+                .ConfigureServices((host, services) =>
+                {
+                    services.AddHostedService<DbTest>();
+                    services.UseEFCoreSharding(config =>
+                    {
+                        config.UseDatabase(conString, DatabaseType.SQLite);
+                        //使用多个数据库
+                        config.UseDatabase<IMyRepository>(conString, DatabaseType.SQLite);
+                    });
+                })
+                .Build()
+                .Run();
+        }
+    }
+}
