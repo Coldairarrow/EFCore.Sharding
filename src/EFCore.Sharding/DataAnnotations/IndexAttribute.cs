aff02774dc7e7869df1a94e0cb275b319f18ae8d
Coldairarrow <862520575@qq.com>
2020-04-22 22:02:44 +0800

    复合主键支持

---
+++ src/EFCore.Sharding/DataAnnotations/IndexAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EFCore.Sharding.DataAnnotations
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
+    public class IndexAttribute : Attribute
+    {
+        public IndexAttribute()
+        {
+
+        }
+    }
+}
