afd5d5337543f76e550f4ad4f7c66580fe9d5bb6
Coldairarrow <862520575@qq.com>
2020-06-23 22:10:30 +0800

    Demo补充

--- examples/Demo.DI/Program.cs
+++ examples/Demo.DI/Program.cs
@@ -2,6 +2,7 @@
 using EFCore.Sharding.Tests;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System;
 
 namespace Demo.DI
 {
@@ -19,9 +20,22 @@ namespace Demo.DI
                     services.AddHostedService<DbTest>();
                     services.UseEFCoreSharding(config =>
                     {
+                        //单表
                         config.UseDatabase(Config.SQLITE1, DatabaseType.SQLite);
                         //使用多个数据库
                         config.UseDatabase<IMyDbAccessor>(Config.SQLITE1, DatabaseType.SQLite);
+
+                        DateTime startTime = DateTime.Now.AddMinutes(-5);
+                        DateTime endTime = DateTime.MaxValue;
+                        //分表
+                        config.AddAbsDb(DatabaseType.SQLite)//添加抽象数据库
+                            .AddPhysicDbGroup()//添加物理数据库组
+                            .AddPhysicDb(ReadWriteType.Read | ReadWriteType.Write, Config.SQLITE1)//添加物理数据库1
+                            .SetDateShardingRule<Base_UnitTest>(nameof(Base_UnitTest.CreateTime))//设置分表规则
+                            .AutoExpandByDate<Base_UnitTest>(//设置为按时间自动分表
+                                ExpandByDateMode.PerMinute,
+                                (startTime, endTime, ShardingConfig.DefaultDbGourpName)
+                                );
                     });
                 })
                 .Build()
