1a9a64dfd7b3fbb739e7804db9874c1de033d493
Dishone <Dishonek@outlook.com>
2021-06-04 13:03:04 +0800

    6.0 Preview 4

--- examples/Demo.HelloWorld/Program.cs
+++ examples/Demo.HelloWorld/Program.cs
@@ -21,7 +21,7 @@ namespace Demo.HelloWorld
             {
                 config.SetEntityAssemblies(typeof(Base_UnitTest).Assembly);
 
-                config.UseDatabase(Config.SQLITE1, DatabaseType.SQLite);
+                config.UseDatabase(Config.CONSTRING1, DatabaseType.SqlServer);
             });
             var serviceProvider = services.BuildServiceProvider();
             new EFCoreShardingBootstrapper(serviceProvider).StartAsync(default).Wait();
@@ -30,6 +30,7 @@ namespace Demo.HelloWorld
             //拿到注入的IDbAccessor即可进行所有数据库操作
             var db = scop.ServiceProvider.GetService<IDbAccessor>();
             var logger = scop.ServiceProvider.GetService<ILogger<Program>>();
+
             while (true)
             {
                 await db.InsertAsync(new Base_UnitTest
@@ -41,10 +42,12 @@ namespace Demo.HelloWorld
                     UserName = Guid.NewGuid().ToString()
                 });
                 var count = await db.GetIQueryable<Base_UnitTest>().CountAsync();
+                
+                var data = await db.GetIQueryable<Base_UnitTest>().ToListAsync();
 
                 logger.LogWarning("当前数量:{Count}", count);
 
-                await Task.Delay(1000);
+                //await Task.Delay(1000);
             }
         }
     }
