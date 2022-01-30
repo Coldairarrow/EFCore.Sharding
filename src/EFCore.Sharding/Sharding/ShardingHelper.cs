1b4cda078915f512effa8b8457c0f5c61058483b
Coldairarrow <862520575@qq.com>
2022-01-30 13:20:43 +0800

    按时间分表 nullable类型修复

--- src/EFCore.Sharding/Sharding/ShardingHelper.cs
+++ src/EFCore.Sharding/Sharding/ShardingHelper.cs
@@ -34,9 +34,17 @@ namespace EFCore.Sharding
             }
             protected bool IsParamter(Expression expression)
             {
-                return expression is MemberExpression member
-                    && member.Expression.Type == _rule.EntityType
-                    && member.Member.Name == _rule.ShardingField;
+                //nullable类型转换
+                if (expression is UnaryExpression unaryExpression && unaryExpression.NodeType == ExpressionType.Convert)
+                {
+                    return IsParamter(unaryExpression.Operand);
+                }
+                else
+                {
+                    return expression is MemberExpression memberExpression
+                    && memberExpression.Expression.Type == _rule.EntityType
+                    && memberExpression.Member.Name == _rule.ShardingField;
+                }
             }
             protected bool IsConstant(Expression expression)
             {
