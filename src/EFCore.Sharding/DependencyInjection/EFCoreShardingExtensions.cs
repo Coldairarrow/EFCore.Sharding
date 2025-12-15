8763ccd9884ad65e188787ba61d3e85ea269d01c
colder <862520575@qq.com>
2025-12-15 21:09:03 +0800

    编译修复

--- src/EFCore.Sharding/DependencyInjection/EFCoreShardingExtensions.cs
+++ src/EFCore.Sharding/DependencyInjection/EFCoreShardingExtensions.cs
@@ -1,5 +1,4 @@
-﻿using EFCore.Sharding.DependencyInjection;
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.DependencyInjection;
 using System;
 
 namespace EFCore.Sharding
